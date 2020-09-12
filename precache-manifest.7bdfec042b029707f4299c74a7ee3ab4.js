f217223e778052c104118a34d9a67892db3e6553
moa-mel <52823454+moa-mel@users.noreply.github.com>
2020-09-13 00:13:36 +0100

    Updates

---
+++ precache-manifest.7bdfec042b029707f4299c74a7ee3ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e023a4cab8a9f960fe215779a5ec4f81",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "0de6a18be4aa34d0ff72",
+    "url": "/first-react-app/static/css/main.7e1cf565.chunk.css"
+  },
+  {
+    "revision": "82cfb8f7ca178e59503e",
+    "url": "/first-react-app/static/js/2.8e080fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.8e080fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de6a18be4aa34d0ff72",
+    "url": "/first-react-app/static/js/main.58de6d59.chunk.js"
+  },
+  {
+    "revision": "9b25d4ab4ca46b4c3b7f",
+    "url": "/first-react-app/static/js/runtime-main.820c6f14.js"
+  }
+]);
